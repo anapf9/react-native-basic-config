f0d6fc1defcb50dca37b60fb6ffdfa73b033a1c1
Ana <ana.eng.dev@gmail.com>
2020-06-15 21:44:13 -0300

    configure axios, componentes: flatList and SafeAreaView

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api'
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response.data)
+      setProjects(response.data)
+    })
+  }, []) 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <View style={styles.container}>
-        <Text style={styles.title}> Oiii meu amor. By: seu Príncipe </Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.title} >{item.title}!!</Text>
+          )}
+          >
+        </FlatList>
+      </SafeAreaView>
+      {/* <View style={styles.container}>
+        <Text style={styles.title}> Testando </Text>
+          {projects.map(project => (
+            <Text style={styles.title} key={project.id}>{project.title}</Text>
+          ))}
+      </View> */}
     </>
   );
 }
@@ -15,8 +38,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+    // justifyContent: 'center',
+    // alignItems: 'center'
   },
 
   title: {
